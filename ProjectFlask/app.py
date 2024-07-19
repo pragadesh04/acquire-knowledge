4f508b5c69553742fe27a538091e9045ab9841e3
Praga866 <pragaofficial004@gmail.com>
2024-07-19 20:35:23 +0530

    Flask basics added

--- ProjectFlask/app.py
+++ ProjectFlask/app.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
 @app.route('/')
+@app.route('/index')
 def index():
-    return '<h1>_praga_always_smart_</h1><p>be active not proactive</p>'
-
-name = "home"
+    titles = ["C","Java","Python","JavaScripts","Flask","end"]
+    logged = False
+    return render_template("index.html", titles = titles, logged = logged)
 
 @app.route('/home/<name>')
 def home(name):
-    return "<h1>this is home:{}</h1>".format(name.upper())
+    # return "<h1>this is home</h1>"
+    names = ["apple", "orange", "pineapple"]
+    profile = {"name":name, "age":23}
+    return render_template("home.html", name = name, names=names, profile = profile)
+
 
 
 if __name__ == '__main__':
